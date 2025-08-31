2ae25dd5f9407fd4a0c012a9db77343c896754cd
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2025-08-31 13:48:36 +0530

    Add Testimonials section of Landing Page

---
+++ quick-desk-frontend/src/components/pages/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../features/Hero'
+import Features from '../features/Features'
+import Testimonial from '../features/Testimonial'
+import Footer from '../features/Footer'
+
+const LandingPage = () => {
+  return (
+    <div className='relative h-full'>
+        <Hero />
+        <Features />
+        <Testimonial />
+        <Footer />
+    </div>
+  )
+}
+
+export default LandingPage
